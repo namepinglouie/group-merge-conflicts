a704c7142594c99b846a6830af2466849444a6d1
ERIAN CABALLERO <eriancaballero@ERIANs-MBP.home>
2021-12-02 16:00:35 -0500

    minor change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function fn(...c) {
+function tabs(...c) {
     if (!c.every((a) => Number.isNumber(a)))
       throw "All arguments must be numbers.";
     return c.reduce((a, b) => a + b);
   }
   
-  fn(1, 2, 3); //> 6
-  fn(10, "B", 20); //> error All arguments must be numbers.
+  tabs(1, 2, 3); //> 6
+  tabs(10, "B", 20); //> error All arguments must be numbers.
   
