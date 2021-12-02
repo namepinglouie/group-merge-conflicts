4b30a7f23fa24132e4e0be1a0a5dc9beb4447c3f
Christina Loiacono <loiaconochristina@gmail.com>
2021-12-02 16:00:41 -0500

    changes made to variables

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function fn(...c) {
-    if (!c.every((a) => Number.isNumber(a)))
+function fn(...cee) {
+    if (!cee.every((ay) => Number.isNumber(ay)))
       throw "All arguments must be numbers.";
-    return c.reduce((a, b) => a + b);
+    return cee.reduce((ay, bee) => ay + bee);
   }
   
   fn(1, 2, 3); //> 6
   fn(10, "B", 20); //> error All arguments must be numbers.
+
+  console.log("Jenna is taking a nap.")
   
